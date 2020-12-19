2bf5c8ddbc5cd2b02e1ca646e6eab6061fc4ee2d
Eduard <eduardbargues@gmail.com>
2020-12-19 11:57:54 +0100

    Ejercicio 2

--- server.js
+++ server.js
@@ -8,38 +8,43 @@ const {
 const pool = new Pool({
     user: "eduar",
     host: "localhost",
-    database: "",
-    password: "",
+    database: "20201219_migracode",
+    password: "1459#Adei",
     port: 5432,
 });
 
-// Follow the above steps to create a new POST endpoint /hotels to create a new hotel.
-// Make sure to add validation for the number of rooms and the hotel name. 
-// Test your new API endpoint with Postman and check that the new hotel has been correctly created in your database.
-
-// Add a new POST API endpoint to create a new customer in the customers table.
-
-// Add validation to check that there is no other customer with the same name in the customers table before creating a new customer
-
+// ENDPOINTS
 // Add the GET endpoints /hotels and /hotels/:hotelId mentioned above and try to use these endpoints with Postman.
-
 // Add a new GET endpoint /customers to load all customers ordered by name.
-
 // Add a new GET endpoint /customers/:customerId to load one customer by ID.
-
-// Add a new GET endpoint /customers/:customerId/bookings to load all the bookings of a specific customer. 
-// Returns the following information: check in date, number of nights, hotel name, hotel postcode.
-
-// Add the PUT endpoint /customers/:customerId and verify you can update a customer email using Postman.
-
-// Add validation for the email before updating the customer record in the database. If the email is empty, return an error message.
-
-// Add the possibility to also update the address, the city, the postcode and the country of a customer. 
-// Be aware that if you want to update the city only for example, the other fields should not be changed!
-
-// Add the DELETE endpoint /customers/:customerId above and verify you can delete a customer along their bookings with Postman.
-
-// Add a new DELETE endpoint /hotels/:hotelId to delete a specific hotel. 
-// A hotel can only be deleted if it doesn't appear in any of the customers' bookings! Make sure you add the corresponding validation before you try to delete a hotel.
+// Add a new GET endpoint /customers/:customerId/bookings to load all the bookings of a specific customer. Returns the following information: check in date, number of nights, hotel name, hotel postcode.
+app.get("/hotels/:hotelId", function (req, res) {
+    const hotelId = req.params.hotelId;
+    pool
+        .query("SELECT * FROM hotels WHERE id=$1", [hotelId])
+        .then((result) => res.json(result.rows))
+        .catch((e) => console.error(e));
+});
+app.get("/customers", function (req, res) {
+    let query = `SELECT * FROM customers ORDER BY name`;
+    pool
+        .query(query)
+        .then((result) => res.json(result.rows))
+        .catch((e) => console.error(e));
+});
+app.get("/customers/:customerId", function (req, res) {
+    const customerId = req.params.customerId;
+    pool
+        .query("SELECT * FROM customers WHERE id=$1", [customerId])
+        .then((result) => res.json(result.rows))
+        .catch((e) => console.error(e));
+});
+app.get("/customers/:customerId/bookings", function (req, res) {
+    const customerId = req.params.customerId;
+    pool
+        .query(`SELECT bookings.checkin_date, bookings.nights, hotels.name, hotels.postcode FROM bookings INNER JOIN hotels ON hotels.id = bookings.hotel_id where bookings.customer_id = $1`, [customerId])
+        .then((result) => res.json(result.rows))
+        .catch((e) => console.error(e));
+})
 
 app.listen(port, () => console.log(`Server listening to port=${port}`))
